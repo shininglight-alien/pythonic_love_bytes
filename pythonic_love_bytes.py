5da110a1828fce9a39a8fa315fe3f4ba3c2811e1
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-02-07 02:56:28 +0800

    having troubles

---
+++ pythonic_love_bytes.py
@@ -0,0 +1,24 @@
+import glob
+import imagehash
+from PIL import Image
+
+my_img_url = r"C:\Users\Ellaine\Downloads\eleyn\ellaine1.jpg"
+my_hash = imagehash.average_hash(Image.open(my_img_url))
+
+sheyn = glob.glob('./sheyn/*.jpg')
+selected = sheyn[0]
+accepted_diff = 1000
+for img in sheyn:
+    img_hash = imagehash.average_hash(Image.open(img))
+    diff = img_hash - my_hash
+    if diff < accepted_diff:
+        selected = img
+        accepted_diff = diff
+
+eleyn_img = Image.open(my_img_url)
+sheyn_img = Image.open(selected)
+couple_img = Image.new('RGB', (eleyn_img.width + sheyn_img.width, eleyn_img.height))
+couple_img.paste(eleyn_img, (0, 0))
+couple_img.paste(sheyn_img, (eleyn_img.width, 0))
+couple_img.save('my_valentine_day_date.jpg')
+couple_img.show()
